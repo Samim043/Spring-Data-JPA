5fee75927b48c446109a9ab9fc5e7dea84b7d7b8
Md. Samim Akhter <srsamim043@gmail.com>
2022-10-10 17:49:46 +0600

    Added Course and Course Material

---
+++ src/main/java/com/sam/spring/jpa/repository/CourseMaterialRepository.java
@@ -0,0 +1,9 @@
+package com.sam.spring.jpa.repository;
+
+import com.sam.spring.jpa.entity.CourseMaterial;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CourseMaterialRepository extends JpaRepository<CourseMaterial, Long> {
+}
---
+++ src/main/java/com/sam/spring/jpa/repository/CourseRepository.java
@@ -0,0 +1,9 @@
+package com.sam.spring.jpa.repository;
+
+import com.sam.spring.jpa.entity.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CourseRepository extends JpaRepository<Course, Long> {
+}
