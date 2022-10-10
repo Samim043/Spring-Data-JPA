f786173e6aac527d0acbc3ffc464c257085fd87f
Md. Samim Akhter <srsamim043@gmail.com>
2022-10-10 19:01:07 +0600

    Added repositories for Course and Course Material

---
+++ src/test/java/com/sam/spring/jpa/repository/CourseRepositoryTest.java
@@ -0,0 +1,24 @@
+package com.sam.spring.jpa.repository;
+
+import com.sam.spring.jpa.entity.Course;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class CourseRepositoryTest {
+
+    @Autowired
+    private CourseRepository courseRepository;
+    
+    @Test
+    public void printAllCourses(){
+        List<Course> courses = courseRepository.findAll();
+        System.out.println("courses = " + courses);
+    }
+
+}
